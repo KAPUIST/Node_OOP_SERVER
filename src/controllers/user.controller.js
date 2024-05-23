5cb290eeaa2d534c3fda858147c7e7385c9ca92f
KAPUIST <91464689+KAPUIST@users.noreply.github.com>
2024-05-23 20:40:03 +0900

    Feat - 사용자 인증

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,18 +1,18 @@
 import { AsyncErrorHandler } from "../middlewares/asyncError.middleware.js";
 import STATUS_CODES from "../utils/statusCode.js";
-import ErrorHandler from "../utils/errorHandler/errorHandler.js";
+
+import { getUserById } from "../services/user.service.js";
 
 //유저 생성 컨트롤러
 export const getUser = AsyncErrorHandler(async (req, res, next) => {
   try {
+    const user = await getUserById(req.user.user_id);
+
     res.status(STATUS_CODES.OK).json({
       statusCode: STATUS_CODES.OK,
       data: user
     });
   } catch (error) {
-    if (error.isJoi) {
-      next(new ErrorHandler(STATUS_CODES.BAD_REQUEST, error.message));
-    }
     next(error);
   }
 });
